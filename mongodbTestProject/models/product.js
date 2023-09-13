3fddf3efc1b1762730f7a196d262362c0a2c0d8c
dennis <dennisjshofficial@gmail.com>
2023-09-13 23:43:17 +0530

    made the project functional

--- mongodbTestProject/models/product.js
+++ mongodbTestProject/models/product.js
@@ -1,17 +1,26 @@
 const getDb = require('../util/database').getDb;
+const mongodb = require('mongodb');
 
 class Product {
-  constructor(title, price, imageUrl, description){
+  constructor(title, price, imageUrl, description, id){
     this.title = title;
     this.price = price;
     this.imageUrl = imageUrl;
     this.description = description;
+    this._id = new mongodb.ObjectId(id);
   }
 
   save(){
     const db = getDb();
-    return db.collection('products')
-    .insertOne(this)
+    let dbOp;
+    if(this._id){
+      dbOp = db
+      .collection('products')
+      .updateOne({_id: (this._id)}, {$set: this}); //telling mongodb to which collection to collect to...
+    } else{
+      dbOp = db.collection('products').insertOne(this);
+    }
+    return dbOp
     .then((result)=> {
       console.log(result)
     })
@@ -19,6 +28,48 @@ class Product {
       console.log(err)
     })
   }
+
+  static fetchAll(){
+    const db = getDb();
+    return db.collection('products')
+    .find()
+    .toArray()
+    .then((result)=>{
+      console.log(result);
+      return result;
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
+
+  static findById(prodId){
+    const db = getDb();
+    return db.collection('products')
+    .find({_id: new mongodb.ObjectId(prodId)})
+    .next()
+    .then((product)=>{
+      console.log(product);
+      return product;
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  }
+
+  static deleteById(prodId){
+    const db = getDb();
+    return db.collection('products')
+    .deleteOne({_id: new mongodb.ObjectId(prodId)})
+    .then((result)=>{
+      console.log('deleted');
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  }
+
+
 }
 
 module.exports = Product;
