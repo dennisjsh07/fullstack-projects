288c4f710608f94509423e7c8aef6542660ce2af
dennis <dennisjshofficial@gmail.com>
2023-09-15 10:45:45 +0530

    cart added

--- mongodbTestProject/app.js
+++ mongodbTestProject/app.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('6503cbd99e3f45ad53797446')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
--- mongodbTestProject/models/user.js
+++ mongodbTestProject/models/user.js
@@ -1,12 +1,14 @@
 const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
-const objectId = mongodb.ObjectId;
+const ObjectId = mongodb.ObjectId;
 
 class User{
-  constructor(username, email){
+  constructor(username, email, cart, id){
     this.username = username;
-    this.email = email
+    this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
 
   save(){
@@ -14,11 +16,41 @@ class User{
     return db.collection('users').insertOne(this);
   }
 
+  addToCart(product){
+    const cartProductIndex = this.cart.items.findIndex((cp)=>{
+      return cp.productId.toString() === product._id.toString();
+    });
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+
+    if(cartProductIndex >= 0){
+      newQuantity = this.cart.items[cartProductIndex].quantity+1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else{
+      updatedCartItems.push({
+        productId: new ObjectId(product._id), 
+        quantity: newQuantity
+      });
+    }
+    
+    const updatedCart = {
+      items: updatedCartItems
+    };
+    const db = getDb();
+    return db
+    .collection('users')
+    .updateOne(
+      {_id: new mongodb.ObjectId(this._id)},
+      {$set: {cart: updatedCart}}
+      );
+
+  }
+
   static findById(userId){
     const db = getDb();
     return db
     .collection('users')
-    .findOne({_id: new objectId(userId)})
+    .findOne({_id: new ObjectId(userId)})
     .then((user)=>{
       console.log(user);
       return user;
